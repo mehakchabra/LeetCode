468b5341bb5b3a8e63aaed4de558eefdfdade352
Mehak Chabra <86364580+mehakchabra@users.noreply.github.com>
2023-09-04 10:12:44 +0530

    Time: 145 ms (89.43%), Space: 17.8 MB (95.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,13 +1,25 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count = {}
-        res, maxCount = 0,0
+        # Boyer - moore algorithm
+        res, count = 0,0
 
         for n in nums:
-            count[n] = count.get(n,0) + 1
-            res = n if count[n] > maxCount else res
-            maxCount = max(count[n], maxCount)
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
         return res
+
+
+
+
+        # count = {}
+        # res, maxCount = 0,0
+
+        # for n in nums:
+        #     count[n] = count.get(n,0) + 1
+        #     res = n if count[n] > maxCount else res
+        #     maxCount = max(count[n], maxCount)
+        # return res
                 
         
         
