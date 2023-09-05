776dc52e4cbd23456b0a3b2fbc955cb1b8c491b1
Mehak Chabra <86364580+mehakchabra@users.noreply.github.com>
2023-09-05 13:29:36 +0530

    Time: 54 ms (11.81%), Space: 16.3 MB (30.52%) - LeetHub

--- 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
+++ 2595-number-of-even-and-odd-bits/2595-number-of-even-and-odd-bits.py
@@ -4,8 +4,8 @@ class Solution:
         odd = 0
         binary = (bin(n)[2:])[::-1]
         for i,v in enumerate(binary):
-            if i%2 == 0 and v == "1": even += 1
-            if i%2 != 0 and v == "1": odd += 1
+            if v == "1" and i%2 == 0: even += 1
+            if v == "1" and i%2 != 0: odd += 1
         return [even, odd]
 
         
