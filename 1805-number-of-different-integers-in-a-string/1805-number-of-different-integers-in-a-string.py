aaec95ba0946956f211c067e75ab394cb4a3f6ea
Mehak Chabra <86364580+mehakchabra@users.noreply.github.com>
2023-11-21 15:52:55 +0530

    Time: 31 ms (93.80%), Space: 16.3 MB (41.36%) - LeetHub

---
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numDifferentIntegers(self, word: str) -> int:
+        nums='0123456789'
+
+        for i in range(len(word)):
+            if word[i] not in nums:
+                word= word.replace(word[i],' ')
+        num_list=word.split()
+
+        res=[]
+        for i in range(len(num_list)):
+            res.append(int(num_list[i]))
+
+        return len(set(res)) 
