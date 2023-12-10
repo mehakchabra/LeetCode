df49dcc4bf237053fdac00ee281769724fad264f
Mehak Chabra <86364580+mehakchabra@users.noreply.github.com>
2023-12-10 13:02:18 +0530

    Time: 46 ms (48.45%), Space: 16.4 MB (43.47%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ans = {}
+        for index, val in enumerate(order):
+            ans[val] = index
+
+        for i in range(len(words) - 1):
+            for j in range(len(words[i])):
+
+                if j >= len(words[i + 1]): 
+                    return False
+
+                if words[i][j] != words[i + 1][j]:
+                    if ans[words[i][j]] > ans[words[i + 1][j]]: return False
+                   
+                    break
+
+        return True
